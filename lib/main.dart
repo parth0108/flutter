5b7b5d439708ac8109022da157880ff220c74400
parth0108 <parthamar0052@gmail.com>
2021-09-03 10:21:08 +0530

    Date
    First Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:widget/FlutterLifeCycle/flutter_life_cycle_understand.dart';
+import 'package:widget/GrideViewExample.dart';
+import 'package:widget/Silver_properties/silver_app_bar.dart';
+import 'package:widget/Stack/stack_properties.dart';
 import 'package:widget/adaptiveui/aspectRatio.dart';
 import 'package:widget/adaptiveui/fittedbox.dart';
 import 'package:widget/adaptiveui/fractional.dart';
 import 'package:widget/adaptiveui/layoutbuilder.dart';
 import 'package:widget/adaptiveui/mediaquery.dart';
 import 'package:widget/adaptiveui/oriantationbuilder.dart';
+import 'package:widget/advance%20/global_key.dart';
+import 'package:widget/advance%20/stream.dart';
 import 'package:widget/firebase/Firebase_example.dart';
 import 'package:widget/menuiteam.dart';
 import 'package:widget/horizontalLIstView.dart';
@@ -12,11 +18,10 @@ import 'package:widget/radio_button_list.dart';
 import 'package:widget/theamChange/theamChange.dart';
 import 'package:widget/video_player/splashscreen.dart';
 import 'bottomnavigation/mainfile.dart';
-import 'game /game_practicle.dart';
+import 'game/game_practicle.dart';
 import 'google map/googlemap.dart';
 import 'instaDemo.dart';
 import 'video_player/videoplayer.dart';
-import 'widget/BlockPattern /counter.dart';
 
 void main() {
   runApp(MyApp());
@@ -44,7 +49,8 @@ class _MyAppState extends State<MyApp> {
      //home:FittedBoxPage()/*MyStatefulWidget(),*/
       //home:MyHomePage()/*MyStatefulWidget(),*/
       //home:FireBaseExmple()/*MyStatefulWidget(),*/
-     home:Tic_Tac_Toe_Practicle()/*MyStatefulWidget(),*/
+     home:
+     Silver_app_bar()/*MyStatefulWidget(),*/
 
 
     );
